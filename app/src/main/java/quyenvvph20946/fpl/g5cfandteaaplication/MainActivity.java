c706b7f1e10057cb95310f5d3e86944dc32fc358
quyen <quyenvvph20946@fpt.edu.vn>
2023-03-16 15:08:13 +0700

    second commit

---
+++ app/src/main/java/quyenvvph20946/fpl/g5cfandteaaplication/MainActivity.java
@@ -0,0 +1,14 @@
+package quyenvvph20946.fpl.g5cfandteaaplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
